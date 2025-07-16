be3eae729d6715b02e6fc8e174c2524b233a9fc7
Dipesh Dhungana <dipeshdhungana2002@gmail.com>
2025-07-16 12:05:56 +0545

    selecting the piece

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -21,6 +21,18 @@ class _GameBoardState extends State<GameBoard> {
     _initializeBoard();
   }
 
+  //seleted piece currently on the chess board
+  // if no piece selected then it will be null
+  ChessPiece? selectedPiece;
+  //keeping track of the row/column of the selected piece
+  //Default Value as nothing has been selected
+  int selectedRow = -1;
+  int selectedCol = -1;
+
+  //A list of valid moves for currently selected piece
+  //each move is represented as a list with two elements
+  List<List<int>> validMoves = [];
+
   //initialize Board
   void _initializeBoard() {
     //initialize the board with nulls, meaning no pieces in those positions
@@ -28,6 +40,7 @@ class _GameBoardState extends State<GameBoard> {
       8,
       (index) => List.generate(8, (index) => null),
     );
+
     //place pawns  (we need 8)
     for (int i = 0; i < 8; i++) {
       newBoard[1][i] = ChessPiece(
@@ -41,6 +54,7 @@ class _GameBoardState extends State<GameBoard> {
         imagePath: "assets/pawn.png",
       );
     }
+
     //place rooks
     newBoard[0][0] = ChessPiece(
       type: ChessPieceType.rook,
@@ -60,81 +74,97 @@ class _GameBoardState extends State<GameBoard> {
     newBoard[7][7] = ChessPiece(
       type: ChessPieceType.rook,
       isWhite: true,
-      imagePath: "assets/knight.png",
+      imagePath: "assets/rook.png",
     );
+
     //place knights
     newBoard[0][1] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.knight,
       isWhite: false,
       imagePath: "assets/knight.png",
     );
     newBoard[0][6] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.knight,
       isWhite: false,
       imagePath: "assets/knight.png",
     );
     newBoard[7][1] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.knight,
       isWhite: true,
       imagePath: "assets/knight.png",
     );
     newBoard[7][6] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.knight,
       isWhite: true,
       imagePath: "assets/knight.png",
     );
+
     //place bishops
     newBoard[0][2] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.bishop,
       isWhite: false,
       imagePath: "assets/bishop.png",
     );
     newBoard[0][5] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.bishop,
       isWhite: false,
       imagePath: "assets/bishop.png",
     );
     newBoard[7][2] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.bishop,
       isWhite: true,
       imagePath: "assets/bishop.png",
     );
     newBoard[7][5] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.bishop,
       isWhite: true,
       imagePath: "assets/bishop.png",
     );
+
     //place queens
     newBoard[0][3] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.queen,
       isWhite: false,
       imagePath: "assets/queen.png",
     );
     newBoard[7][4] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.queen,
       isWhite: true,
       imagePath: "assets/queen.png",
     );
+
     //place kings
     newBoard[0][4] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.king,
       isWhite: false,
       imagePath: "assets/king.png",
     );
     newBoard[7][3] = ChessPiece(
-      type: ChessPieceType.rook,
+      type: ChessPieceType.king,
       isWhite: true,
       imagePath: "assets/king.png",
     );
+
     board = newBoard;
   }
 
-  // //create a piece
-  // ChessPiece myPawn = ChessPiece(
-  //   type: ChessPieceType.pawn,
-  //   isWhite: true,
-  //   imagePath: 'assets/pawn_white.png',
-  // );
+  //user selected a piece
+  void pieceSelected(int row, int col) {
+    setState(() {
+      //selected a piece if there is a piece in that position
+      if (board[row][col] != null) {
+        selectedPiece = board[row][col];
+        selectedRow = row;
+        selectedCol = col;
+      }
+      //if a piece is selected, calculate its valid moves
+      // validMoves = calculateRawValidMoves();
+    });
+  }
+
+  //Calculate the Real Valid Moves
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -149,7 +179,16 @@ class _GameBoardState extends State<GameBoard> {
           //get the row and column position of this square
           int row = index ~/ 8;
           int col = index % 8;
-          return Square(isWhite: isWhite(index), piece: board[row][col]);
+
+          //check if this square is selected or not
+          bool isSelected = selectedRow == row && selectedCol == col;
+
+          return Square(
+            isWhite: isWhite(index),
+            piece: board[row][col],
+            isSelected: isSelected,
+            onTap: () => pieceSelected(row, col),
+          );
         },
       ),
     );
